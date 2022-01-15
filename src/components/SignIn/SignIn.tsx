e6e23885fc2006a1c51d6a132e31d2bf1812f94c
Pablo Suarez <pablo.suarez@imagineapps.co>
2022-01-15 14:39:40 -0500

    Estructura general

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './SignIn.module.css';
+
+const SignIn = () => (
+  <div className={styles.SignIn}>
+    SignIn Component
+  </div>
+);
+
+export default SignIn;
