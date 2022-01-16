cfd652ce02b2d09eecdda23cbd32cdf783750906
Pablo Suarez <pablo.suarez@imagineapps.co>
2022-01-15 22:33:40 -0500

    useInputText custom hook

---
+++ src/hooks/useInputText/useInputText.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const useInputText = (initialValue: any) => {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    setValue,
+    reset: () => setValue(""),
+    bind: {
+      value,
+      onChange: (event: any) => {
+        setValue(event.target.value);
+      }
+    }
+  };
+};
+
+export default useInputText;
